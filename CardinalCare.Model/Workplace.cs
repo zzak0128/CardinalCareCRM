6d4d411adac30c61f6259872963212edb8695558
Zachary Zak <Zach.Zak@devzapps.com>
2022-01-16 13:25:50 -0600

    Add Workplace class and AddWorkplace component

---
+++ CardinalCare.Model/Workplace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CardinalCare.Model
+{
+    public class Workplace
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Number { get; set; }
+
+        public Workplace()
+        {
+        }
+    }
+}
