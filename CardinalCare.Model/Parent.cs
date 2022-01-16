e55a4459f87b575ff3eecb2bb4cb31d8651ce414
Zachary Zak <Zach.Zak@devzapps.com>
2022-01-16 12:11:52 -0600

    Add props to parent shell class

--- CardinalCare.Model/Parent.cs
+++ CardinalCare.Model/Parent.cs
@@ -3,6 +3,12 @@ namespace CardinalCare.Model
 {
     public class Parent
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // Address prop
+        public string PhoneNumber { get; set; }
+        // Workplace prop
+
         public Parent()
         {
         }
